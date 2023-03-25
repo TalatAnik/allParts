312b9198a1730ff40c0d9e5228d2cc96a288bf7b
Talat Anik <talat_anik@hotmail.com>
2023-03-25 08:41:56 +0600

    single product extraction complete

--- scrapePage.js
+++ scrapePage.js
@@ -11,19 +11,22 @@ async function main (searchString) {
   await inputField.type(searchString || '123')
   const submitButton = await page.$('input[type="submit"][value="Search"]')
   await submitButton.click()
-  await page.waitForNavigation()
+  await page.waitForNavigation({ waitUntil: 'networkidle2' })
   
-  // Check if div with id "partDescription" exists
-  const partDescriptionDiv = await page.$('#partDescription')
-  if (partDescriptionDiv) {
-    const textContent = await page.evaluate(element => element.textContent, partDescriptionDiv)
-    console.log(textContent)
-  }
+  
   
   // Check if div with class "partNumber" exists
   const partNumberDiv = await page.$('.partNumber')
   if (partNumberDiv) {
     const textContent = await page.evaluate(element => element.textContent, partNumberDiv)
+    console.log(textContent)
+  }
+
+
+  // Check if div with id "partDescription" exists
+  const partDescriptionDiv = await page.$('#partDescription')
+  if (partDescriptionDiv) {
+    const textContent = await page.evaluate(element => element.textContent, partDescriptionDiv)
     console.log(textContent)
   }
   
@@ -36,6 +39,37 @@ async function main (searchString) {
     console.log(weightText)
   }
   
+  // Check if div with id "attContainer" exists
+  const attContainerDiv = await page.$('#attContainer')
+  if (attContainerDiv) {
+    // Find all the <p> elements within the div with id "attContainer"
+    const pElements = await page.$$('#attContainer>p')
+    let textContents = []
+    for (let i = 0; i < pElements.length; i++) {
+      const textContent = await page.evaluate(element => element.innerText, pElements[i])
+      textContents.push(textContent)
+    }
+
+    let productDescription = textContents.join('<br>').replace(/\s{2,}/g, ' ')
+    console.log(productDescription)
+  }
+  
+  // Check if div with id "associatearea" exists
+  const associateAreaDiv = await page.$('#associatearea')
+  if (associateAreaDiv) {
+    // Get all rows starting from the second row
+    const tableRows = await page.$$('#associatearea tr:nth-child(n+2)')
+    let extractedTexts = []
+    for (let i = 0; i < tableRows.length; i++) {
+      // Get the text from the first column
+      const textContent = await page.evaluate(element => element.cells[0].innerText, tableRows[i])
+      extractedTexts.push(textContent)
+    }
+    console.log(extractedTexts.join(', ').replace(/\s{2,}/g, ' '))
+  } else {
+    console.log('null')
+  }
+
   // Ensure all the contents on the page are loaded before looking for the images in the smallimgboxdivs
   await page.waitForSelector('div[id="smallimgbox"]')
   
@@ -57,7 +91,7 @@ async function main (searchString) {
     }
   }
   
-  // await browser.close()
+  await browser.close()
 }
 
 main (process.argv[2])
